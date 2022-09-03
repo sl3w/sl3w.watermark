d43de43f06cdf4b32ce882cb8918324562fd8f46
Leo <33494928+sl3w@users.noreply.github.com>
2022-09-03 20:59:18 +0400

    fix description

--- lang/ru/install/index.php
+++ lang/ru/install/index.php
@@ -1,7 +1,7 @@
 <?php
 
 $MESS['SL3W_WATERMARK_MODULE_NAME'] = 'Водяной знак для инфоблоков. Защита изображений';
-$MESS['SL3W_WATERMARK_MODULE_DESC'] = 'Модуль позволяет в автоматическом режиме добавлять водяной знак на изображения в полях и свойствах элементов инфоблоков';
+$MESS['SL3W_WATERMARK_MODULE_DESC'] = 'Модуль позволяет в автоматическом режиме накладывать водяной (авторский) знак на изображения в полях и свойствах элементов инфоблоков';
 $MESS['SL3W_WATERMARK_PARTNER_NAME'] = 'SL3W';
 $MESS['SL3W_WATERMARK_PARTNER_URI'] = 'https://github.com/sl3w/sl3w.watermark';
 
