e5f6900b906dabf6feff10b88c1d83979253428f
Leo <33494928+sl3w@users.noreply.github.com>
2022-09-10 05:51:42 +0400

    version 1.0.3 - add button to go settings after install

--- lang/ru/install/step.php
+++ lang/ru/install/step.php
@@ -4,3 +4,4 @@ $MESS['SL3W_WATERMARK_STEP_BEFORE'] = 'Модуль';
 $MESS['SL3W_WATERMARK_MODULE_NAME'] = 'Водяной знак для инфоблоков. Защита изображений';
 $MESS['SL3W_WATERMARK_STEP_AFTER'] = 'установлен';
 $MESS['SL3W_WATERMARK_STEP_BACK'] = 'Вернуться в список';
+$MESS['SL3W_WATERMARK_STEP_GO_TO_SETTINGS'] = 'Перейти к настройкам модуля';
